865fa453333d3b4acccd9cee1761593f36af8a58
JubilantMarmot <abboudalomari6@gmail.com>
2024-08-19 21:02:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,46 @@ function calculate() {
 }
 
 function parseExpression(expr) {
-  // THIS is JUST for testing! This is not good to use and wont work with sin cos and tan!
-    return eval(expr);
+    let tokens = expr.match(/\d+|\D+/g);
+    let output = [];
+    let operators = [];
+
+    const precedence = {'+': 1, '-': 1, '*': 2, '/': 2};
+
+    function applyOperator(op, b, a) {
+        switch (op) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+            case '/': return a / b;
+        }
+    }
+
+    for (let token of tokens) {
+        if (!isNaN(token)) {
+            output.push(parseFloat(token));
+        } else if (['+', '-', '*', '/'].includes(token)) {
+            while (operators.length && precedence[operators[operators.length - 1]] >= precedence[token]) {
+                output.push(operators.pop());
+            }
+            operators.push(token);
+        }
+    }
+
+    while (operators.length) {
+        output.push(operators.pop());
+    }
+
+    let stack = [];
+    for (let token of output) {
+        if (typeof token === 'number') {
+            stack.push(token);
+        } else {
+            let b = stack.pop();
+            let a = stack.pop();
+            stack.push(applyOperator(token, b, a));
+        }
+    }
+
+    return stack[0];
 }
